dc03c05c0f559d37b68a44cf5b71d9c2db8206aa
John Tucker <jtucker@spotify.com>
2021-11-01 16:05:04 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = []
+with open('requirements.txt') as f:
+    for line in f:
+        line = line.strip()
+        install_requires.append(line)
+
+setup(
+    entry_points={
+        "console_scripts": [
+            "hellocli.main:cli"
+        ],
+    },
+    install_requires=install_requires,
+    name='hellocli',
+    packages=find_packages('hellocli'),
+    version='0.1.0',
+)
