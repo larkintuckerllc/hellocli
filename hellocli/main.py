e05d541b88d48163940f4332df10a492b4ba21d5
John Tucker <jtucker@spotify.com>
2021-11-02 12:14:47 -0400

    classes

--- hellocli/main.py
+++ hellocli/main.py
@@ -1,9 +1,30 @@
 """Hello CLI."""
 import click
 
+class Dog:
+    def __init__(self, breed):
+        self.breed = breed
+    
+    @classmethod
+    def pug_factory(cls):
+        return cls('pug')
+
+    def bark(self):
+        if self.breed == 'pug':
+            print('bark')
+            return
+        print('BARK')
+
+    def bark_at(self, *, person):
+        self.bark()
+        print(person)
 
 @click.command()
-@click.argument('phrase')
-def cli(phrase):
+@click.argument('breed')
+def cli(breed):
     """Print phrase."""
-    print(phrase)
+    dog = Dog(breed)
+    dog.bark()
+    pug = Dog.pug_factory()
+    pug.bark()
+    pug.bark_at(person='john')
